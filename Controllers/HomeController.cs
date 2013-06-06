938d208e89f7870defd158433438dcb86b3a9b05
Poliana Dias <polianadias@outlook.com>
2013-06-06 01:25:14 -0300

    Initial commit
    Primeiro post do blog usando consulta com parâmetros usando linq.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Show()
+        {
+            //Instânciando a variável retorno para receber o resultado da consulta
+            IEnumerable<Product> retorno;
+            //Instânciando o método de consulta parãmentros
+            var products = new ConsultaProdutosComParamentros();
+            retorno = products.retornaConsultaParamentros();
+            //Passando o retorno para view
+            return View(retorno);
+        }
+
+
+    }
+}
---
+++ Models/ConsultaProdutosComParamentros.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class ConsultaProdutosComParamentros
+    {
+
+        public IEnumerable<Product> retornaConsultaParamentros()
+        {
+            //Instânciado o modelo no EF
+            AdventureWorks2012Entities Model = new AdventureWorks2012Entities();
+
+            //Criando uma lista de string para passar como parãmentros
+            List<string> productNumber = new List<string> { "BA-8327", "BL-2036", "CA-6738", "CN-6137" };
+
+            //Declarando uma variável do tipo IEnumerable para receber o retorno da consulta
+            IEnumerable<Product> ret;
+
+            //Passando para consulta os parãmentros
+            ret = from p in Model.Product
+                  where productNumber.Contains(p.ProductNumber)
+                  select p;
+            return ret;
+        }
+    }
+}
